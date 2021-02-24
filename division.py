ea0404cba91ee629779599198b596c2279a50a31
jasperwreed <jmixon.reed@gmail.com>
2021-02-24 14:50:18 -0800

    playing around with python

---
+++ division.py
@@ -0,0 +1,21 @@
+width = 15
+height = 12.0
+
+print(height / 3)
+
+x = 3
+y = 0
+
+# if statement syntax
+
+# if x == 5:
+#   if y == 0:
+#     print('hello bitch')
+
+# if elif else statement
+if x == 5:
+    print('hello')
+elif y == 0:
+    print('world')
+else:
+    print('hello world')
